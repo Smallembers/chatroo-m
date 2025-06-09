64b58340c74658082f97839a85d3cbb02ffc9e8c
Smallembers <144388153+Smallembers@users.noreply.github.com>
2025-06-09 08:56:34 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(http);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
+});
